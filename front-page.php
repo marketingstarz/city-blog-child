a34a389d4c54b4bfe4a343bbd0af1c04d8c1f17e
k3rnelpan1c442 <marketingstarz@users.noreply.github.com>
2023-08-28 19:41:50 -0600

    Updated homepage code

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+
+get_header();
+
+if ( is_front_page() && is_home() ) {
+	require get_template_directory() . '/home.php';
+} elseif ( is_front_page() && ! is_home() ) {
+	?>
+	<main id="primary" class="site-main">
+		<!-- My Custom Code -->
+		<span>My Custom Code Here</span>
+		<?php require get_template_directory() . '/sections/sections.php'; ?>
+		<?php city_blog_homepage_sections(); ?>
+	</main><!-- #main -->
+	<?php
+}
+
+get_footer();
